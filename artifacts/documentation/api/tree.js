b4574d69c2045090356c1efce157d0c0b46fbfd6
Woodhouse <contact@icecave.com.au>
2013-10-21 03:43:41 +0000

    Publishing artifacts from build 35.

--- artifacts/documentation/api/tree.js
+++ artifacts/documentation/api/tree.js
@@ -2,5 +2,6 @@ var tree = [["Icecave",,"",
                 [["Isolator","Icecave\/Isolator.html","",
                 [["Generator","Icecave\/Isolator\/Generator.html","",
                 [        ]],["Isolator","Icecave\/Isolator\/Isolator.html","",
+                [        ]],["PackageInfo","Icecave\/Isolator\/PackageInfo.html","",
                 [        ]]        ]]        ]]        ]
 
