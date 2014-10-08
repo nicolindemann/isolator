659e79088a0250dfb8f12eec6878ec4e272a94f2
Woodhouse <contact@icecave.com.au>
2014-10-08 01:11:53 +0000

    Publishing artifacts from build 63.

--- artifacts/documentation/api/tree.js
+++ artifacts/documentation/api/tree.js
@@ -4,7 +4,6 @@ var tree = [["Icecave",,"",
                 [["AbstractIsolator","Icecave\/Isolator\/Detail\/AbstractIsolator.html","",
                 [        ]],["Autoloader","Icecave\/Isolator\/Detail\/Autoloader.html","",
                 [        ]],["CodeGenerator","Icecave\/Isolator\/Detail\/CodeGenerator.html","",
-                [        ]]        ]],["IsolatorInterface","Icecave\/Isolator\/IsolatorInterface.html","",
-                [        ]],["PackageInfo","Icecave\/Isolator\/PackageInfo.html","",
+                [        ]]        ]],["PackageInfo","Icecave\/Isolator\/PackageInfo.html","",
                 [        ]]        ]]        ]]        ]
 
